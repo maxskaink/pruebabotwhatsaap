9539e0327dd3fc4b6f52e11febb4ab2227308cd7
Miguel Angel Calambas <calambasvivas11@gmail.com>
2023-07-20 15:47:20 -0500

    Se ha corregido un error con la hora en el servidor

--- Helpers/validations.js
+++ Helpers/validations.js
@@ -1,11 +1,15 @@
 
 const isLaboralTime = () => {
-    const horaInicioLaboral = 7;
-    const horaFinLaboral = 18;
+    const HORA_INICIO = parseInt(process.env.HORA_INICIO)
+    const HORA_FINAL = parseInt(process.env.HORA_FINAL)
+    const CORRECCION_FORMATO_HORA =  parseInt(process.env.CORRECCION_FORMATO_HORA)
+
+    const horaInicioLaboral = HORA_INICIO - CORRECCION_FORMATO_HORA;
+    const horaFinLaboral = HORA_FINAL - CORRECCION_FORMATO_HORA;
   
     const horaActual = new Date().getHours();
 
-    console.log(`La hora el servidor actual es: ${horaActual}`)
+
     if (horaActual >= horaInicioLaboral && horaActual < horaFinLaboral) 
       return false;
      else 
